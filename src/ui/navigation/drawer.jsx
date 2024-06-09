48fd8dd6dfc1b987b05a313fabd1ce343dd1c9b7
sam959 <liekemonster@gmail.com>
2024-06-09 17:58:35 +0200

    drawer opening
    close drawer when leaving its area with cursot

--- src/ui/navigation/drawer.jsx
+++ src/ui/navigation/drawer.jsx
@@ -1,6 +1,30 @@
+"use-client";
 import NavLinks from "./navlinks";
+import { useEffect, useRef } from "react";
 
 export default function Drawer() {
+  const drawerOverlayRef = useRef(null);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const onMouseLeave = () => {
+      console.log("sono uscito");
+      drawerOverlayRef.current.click();
+    };
+
+    // Monto l'event
+    if (drawerOverlayRef?.current && menuRef?.current) {
+      menuRef.current.addEventListener("mouseleave", onMouseLeave);
+    }
+
+    // Lo rimuovo altrimenti la funzione onMouseLeave parte una volta in più
+    return () => {
+      if (drawerOverlayRef?.current) {
+        menuRef.current.removeEventListener("mouseleave", onMouseLeave);
+      }
+    };
+  }, [menuRef, drawerOverlayRef]);
+
   return (
     <div className="drawer">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
@@ -10,8 +34,12 @@ export default function Drawer() {
           htmlFor="my-drawer"
           aria-label="close sidebar"
           className="drawer-overlay"
+          ref={drawerOverlayRef}
         ></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <ul
+          ref={menuRef}
+          className="menu p-4 w-80 min-h-full bg-base-200 text-base-content"
+        >
           <NavLinks />
         </ul>
       </div>
