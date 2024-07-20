7bec5498f011c45aee47883551708f4c463c3fba
sam959 <56480741+sam959@users.noreply.github.com>
2024-07-20 14:45:21 +0200

    update title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,17 @@ import Sidebar from '../ui/navigation/sidebar';
 import SideNav from '../ui/navigation/sidenav';
 import Navbar from '../ui/navigation/navbar';
 import { fira } from '../ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+    title: 'Sam\'s blog',
+    description: 'Samantha Lovisolo\'s personal blog',
+};
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-
 
     return (
         <html lang="en" data-theme="cupcake">
